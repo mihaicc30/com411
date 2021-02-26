70a6b5387032bfb44e33d2a24cfb5a561aaa9ff3
repl.it user <replituser@example.com>
2021-02-26 11:12:13 +0000

    added multiline and simple messages

---
+++ basics/output/multiline_message.py
@@ -0,0 +1,8 @@
+# hashtag is just a comment not run by pyton
+#display message to standard output in multiple lines of code
+print("System failure imminent!")
+print("System reboot has been initiated...\n")
+print("...rebooting sensory system")
+print("...rebooting output motors")
+print("...rebooting hover engine\n")
+print("System online")
--- main.py
+++ main.py
@@ -1,1 +1,4 @@
-print ("hello")
+print("hello")
+print("first step: git add .")
+print("second step: git commit -m \"your message here\"")
+print("last step: git push")
