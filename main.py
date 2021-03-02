24ac590e34028d5e6c57ab57e117af2a85722ff9
repl.it user <replituser@example.com>
2021-03-02 10:39:21 +0000

    another update

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 print("What is your name ?")
 n = input()
+print("Do you have a dog? (types true or false)")
+dog = input()
 
-if len(n) > 9:
+if len(n) > 9 and dog == "True":
   print("You have a very long nameeeeee!")
   print("Your name contains {} letters".format(len(n)))
 else:
   print ("Your name is ok!")
 
 print("This is the END of the program!")
+
+
+
+# t + t = true
+# t + F = false
+# f + f = false
+# f + t = false
