d5d88b74f50dd5c92b8228f866826ab9d017773d
repl.it user <replituser@example.com>
2021-03-04 22:38:55 +0000

    Added code to demonstrate the use of string operators.

---
+++ solo-learning/basics/input/string_operators.py
@@ -0,0 +1,21 @@
+print("\u2701")
+
+
+print("Please enter the number of lives.")
+lives = int(input())
+print("Please enter the energy level.")
+energy = int(input())
+print("Please enter the shield level.")
+shield = int(input())
+
+i = 0
+o = 0
+p = 0
+
+print("Health has been set.")
+print("\n")
+
+print(f"Lives:  {'♥' * lives}")
+print(f"Energy: {'♦' * energy}")
+print(f"Shield: {'♦' * shield}")
+
