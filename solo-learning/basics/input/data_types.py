5c7d44b89449df0bc11ebbabe964cf4a3c7cae85
repl.it user <replituser@example.com>
2021-03-04 21:32:25 +0000

    Added code to demonstrate the use of different data types.

---
+++ solo-learning/basics/input/data_types.py
@@ -0,0 +1,60 @@
+#whole_number = int(input())
+#decimal_number = float(input())
+#string = str(input())
+
+#print("What is your age?")
+#user_age = str(input())
+
+#print("Your age is " + str(user_age))
+
+
+
+#bmi formula = height / weight x2
+
+print("What is your name dude?")
+name = str(input())
+print("How old are you?(in years)")
+age = int(input())
+print("How tall are you?(in meters)")
+height = float(input())
+print("What's your weight?(in kg)")
+weight = float(input())
+
+bmi = float
+bmi = weight / height**2
+print("{} you are {} years old and your bmi is {}.".format(name,age,bmi))
+
+if bmi <= 18.5:
+  print("You are damn skinny. get some meat!")
+elif bmi <= 24.9:
+  print("healthy. good boy, you deserve a bone!")
+elif bmi <= 29.9:
+  print("overweight cow. go exercise!")
+elif bmi >= 30:
+  print("over the limit MUUUUUUUU")
+
+  
+
+
+
+      
+    
+
+#Less than 18.5 = Underweight
+#Between 18.5 - 24.9 = Healthy Weight
+#Between 25 - 29.9 = Overweight
+#Over 30 = Obese
+
+#What is your name human?
+#Prins
+
+#How old are you (in years)?
+#36
+
+#How tall are you (in meters)?
+#1.80
+
+#How much do you weigh (in kilograms)?
+#85
+
+#Prins you are 36 years old and your bmi is 26.23.
