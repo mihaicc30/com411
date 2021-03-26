9eac362d2456b0fb21f5fedb6a62368d2d94107b
repl.it user <replituser@example.com>
2021-03-26 11:28:04 +0000

    how to manipulate and sort a set

--- data/sets/sorted_list.py
+++ data/sets/sorted_list.py
@@ -8,6 +8,7 @@ def observed():
 def remove_observations(observations):
   is_running = True
   while (is_running):
+    observations = []
     print("Do you wish to remove an observation?")
     x = input()
     if x == "yes":
@@ -26,11 +27,11 @@ def run():
   #populate the set
   observations_set = set()
   for observation in observations:
-    data = (observation, observations.count(observation))
+    data = (observation, observations_set.count(observation))
     observations_set.add(data)
   #display the set
   for data in sorted(observations_set):
-    print(f"{data[0]} observed {data[1]} times.")
+    print(f"{data[0]} observed {data[1]}times.")
 
 
 run()
